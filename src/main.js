6298717d163a006ab3ee415ed4f8a9612165a4f8
TomaszLaboj <tomasz.laboj@gmail.com>
2023-08-14 10:58:47 +0100

    added function for time preformance

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
-import { sum } from "./sum.js";
 
-console.log("sum is ", sum(10, 20));
-console.log("This is main.js.");
-console.log("You should probably use `yarn test` or `yarn test --watchAll`");
+function prepareYourInputArray(arrayLength) {
+    const numbersArray = [];
+    
+    for(let i = 0; i<arrayLength ; i++){
+        numbersArray.push(i)
+    }
+    return numbersArray;
+}
+
+function pushFunction(inputArray) {
+    inputArray.push(inputArray.length+1)
+}
+function unshiftFunction(inputArray) {
+    inputArray.unshift(inputArray.length+1)
+}
+function popFunction(inputArray) {
+    inputArray.pop()
+}
+function shiftFunction(inputArray) {
+    inputArray.shift()
+}
+
+
+
+const inputArray = prepareYourInputArray(100000000);
+
+const startTime = performance.now();
+shiftFunction(inputArray);
+const stopTime = performance.now();
+const elapsedTime = stopTime - startTime;
+
+
+
+
+console.log({elapsedTime});
